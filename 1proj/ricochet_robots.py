9d3b6b9e7a5939dec7b54c2f72d118728704d2c3
nfnvsc <nfnvsc@hotmail.com>
2020-10-20 16:18:44 +0100

    Changed board implementation

--- 1proj/ricochet_robots.py
+++ 1proj/ricochet_robots.py
@@ -41,26 +41,55 @@ class Board:
                 self.board[x][y] = {"robot":None, "barriers":[], "target":None}
 
                 if x == 0:
-                    self.board[x][y]["barriers"].append("u")
+                    self._set_barrier("u", (x,y))
                 if x == self.size - 1:
-                    self.board[x][y]["barriers"].append("b")
+                    self._set_barrier("b", (x,y))
                 if y == 0:
-                    self.board[x][y]["barriers"].append("l")
+                    self._set_barrier("l", (x,y))
                 if y == self.size - 1:
-                    self.board[x][y]["barriers"].append("r")
+                    self._set_barrier("r", (x,y))
 
         #append robots
         for r, pos in robots:
-            self._robot_set_position(r, pos)
+            self._set_robot(r, pos)
         
         #append target
         t, pos = target
-        self.board[pos[0] - 1][pos[1] - 1]["target"] = t
-        self.target = target
+        self._set_target(t, pos)
 
         #append barriers
         for b, pos in barriers:
-            self.board[pos[0] - 1][pos[1] - 1]["barriers"].append(b)
+            x, y = pos
+            self._set_barrier(b, (x,y))
+
+    def __position(self, position):
+        return position[0] - 1, position[1] -1
+
+    def __get_position(self, position):
+        x, y = self.__position(position)
+        return self.board[x][y]
+
+    def _get_barriers(self, position):
+        return self.__get_position(position)["barriers"]
+    
+    def _get_robot(self, position):
+        return self.__get_position(position)["robot"]
+    
+    def _get_target(self, position):
+        return self.__get_position(position)["target"]
+
+    def _set_barrier(self, barrier, position):
+        self.__get_position(position)["barriers"].append(barrier)
+
+    def _set_robot(self, robot, position):
+        self.__get_position(position)["robot"] = robot
+
+    def _set_target(self, target, position):
+        self.__get_position(position)["target"] = target
+        self.target = position, target
+    
+    def _reset_robot(self, position):
+        self.__get_position(position)["robot"] = None
             
 
     def robot_position(self, robot: str):
@@ -68,65 +97,51 @@ class Board:
         
         for x in range(self.size):
             for y in range(self.size):
-                if self.board[x][y]["robot"] == robot:
+                if self._get_robot((x, y)) == robot:
                     return (x + 1, y + 1)
 
     def robot_valid_actions(self, robot):
         """ Devolve as possiveis ações do robô passado como argumento. """
         actions = []
 
-        r_x, r_y = self.robot_position(robot)
+        r_x, r_y = self.__position(self.robot_position(robot))
 
         #try up        
-        if ("u" not in self.board[r_x - 1][r_y - 1]["barriers"]) and ("b" not in self.board[r_x - 2][r_y - 1]["barriers"]) \
-            and (self.board[r_x - 2][r_y - 1]["robot"] == None):
+        if ("u" not in self._get_barriers((r_x, r_y))) and ("b" not in self._get_barriers((r_x - 1, r_y))) \
+            and (self._get_robot((r_x - 1, r_y)) == None):
 
             actions.append((robot, "u"))
 
         #try bot
-        if ("b" not in self.board[r_x - 1][r_y - 1]["barriers"]) and ("u" not in self.board[r_x][r_y - 2]["barriers"]) \
-            and (self.board[r_x][r_y - 2]["robot"] == None):
+        if ("b" not in self._get_barriers((r_x, r_y))) and ("u" not in self._get_barriers((r_x + 1, r_y - 1))) \
+            and (self._get_robot((r_x + 1, r_y - 1)) == None):
 
             actions.append((robot, "b"))
 
         #try left
-        if ("l" not in self.board[r_x - 1][r_y - 1]["barriers"]) and ("r" not in self.board[r_x - 1][r_y - 2]["barriers"]) \
-            and (self.board[r_x - 1][r_y - 2]["robot"] == None):
+        if ("l" not in self._get_barriers((r_x, r_y))) and ("r" not in self._get_barriers((r_x, r_y - 1))) \
+            and (self._get_robot((r_x, r_y - 1)) == None):
 
             actions.append((robot, "l"))
 
         #try right
-        if ("r" not in self.board[r_x - 1][r_y - 1]["barriers"]) and ("l" not in self.board[r_x - 1][r_y]["barriers"]) \
-            and (self.board[r_x - 1][r_y]["robot"] == None):
+        if ("r" not in self._get_barriers((r_x, r_y))) and ("l" not in self._get_barriers((r_x , r_y + 1))) \
+            and (self._get_robot((r_x, r_y + 1)) == None):
 
             actions.append((robot, "r"))
         
         return actions
-
-    def _robot_set_position(self, robot, position):
-        x, y = position
-        self.board[x - 1][y - 1]["robot"] = robot
     
-    def _robot_reset_position(self, position):
-        x, y = position
-        self.board[x - 1][y - 1]["robot"] = None
     
     def robot_action(self, action):
         robot, pos = action
 
-        self._robot_reset_position(self.robot_position(robot))
-        self._robot_set_position(robot, pos)
+        self._reset_robot(self.robot_position(robot))
+        self._set_robot(robot, pos)
 
-    """
-    def end(self):
-        self._robot_reset_position(self.robot_position(self.target[0]))
-        self._robot_set_position(*self.target)
-
-    def end_check(self):
-        robot_pos = self.robot_position(self.target[0])
-    """
-    # TODO: outros metodos da classe
-
+    def robot_check_target(self):
+        x, y = self.__position(self.robot_position(self.target[0]))
+        return self._get_target((x,y)) == self._get_robot((x,y))
 
 
 def parse_instance(filename: str) -> Board:
@@ -166,9 +181,6 @@ class RicochetRobots(Problem):
         """ O construtor especifica o estado inicial. """
         self.initial = RRState(board)
 
-        #self.goal = RRState(board)
-        #self.goal.board.end()
-
     def actions(self, state: RRState):
         """ Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento. """
@@ -198,7 +210,7 @@ class RicochetRobots(Problem):
         um estado objetivo. Deve verificar se o alvo e o robô da
         mesma cor ocupam a mesma célula no tabuleiro. """
 
-        return 
+        return state.board.robot_check_target()        
 
     def h(self, node: Node):
         """ Função heuristica utilizada para a procura A*. """
