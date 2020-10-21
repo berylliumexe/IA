9f62052c0a30b64298193c048de4559579c0a322
nfnvsc <nfnvsc@hotmail.com>
2020-10-21 19:32:09 +0100

    minor fixes

--- 1proj/ricochet_robots.py
+++ 1proj/ricochet_robots.py
@@ -2,9 +2,9 @@
 # Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
 # Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
 
-# Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# Grupo 18:
+# 93744 Nuno Carvalho
+# 93739 Miguel Silva
 
 from search import Problem, Node, astar_search, breadth_first_tree_search, \
     depth_first_tree_search, greedy_search
@@ -33,13 +33,14 @@ class Board:
         self.size = size
         self.board = []
         self.target = ()
+        self.robots = {}
 
         #init board
         for x in range(size):
             self.board.append([])
             for y in range(size):
                 self.board[x].append([])
-                self.board[x][y] = {"robot":None, "barriers":[], "target":None}
+                self.board[x][y] = {"robot":None, "barriers":[]}
 
                 if x == 0:
                     self._set_barrier("u", (x,y))
@@ -62,13 +63,24 @@ class Board:
         for b, pos in barriers:
             self._set_barrier(b, self.__position_in(pos))
             if b == "u":
-                self._set_barrier("d", self.__position_in((pos[0] - 1, pos[1])))
+                self._set_barrier("d", self.__position_in(self.__position_change(pos, "u")))
             if b == "d":
-                self._set_barrier("u", self.__position_in((pos[0] + 1, pos[1])))
+                self._set_barrier("u", self.__position_in(self.__position_change(pos, "d")))
             if b == "l":
-                self._set_barrier("r", self.__position_in((pos[0], pos[1] - 1)))
+                self._set_barrier("r", self.__position_in(self.__position_change(pos, "l")))
             if b == "r":
-                self._set_barrier("l", self.__position_in((pos[0], pos[1] + 1)))
+                self._set_barrier("l", self.__position_in(self.__position_change(pos, "r")))
+
+    def __position_change(self, position, direction):
+        """ Returns the translated position according to the input direction """
+        if direction == "u":
+            return (position[0] - 1, position[1])
+        if direction == "d":
+            return (position[0] + 1, position[1])
+        if direction == "l":
+            return (position[0], position[1] - 1)
+        if direction == "r":
+            return (position[0], position[1] + 1)
 
     def __position_out(self, position):
         return (position[0] + 1, position[1] + 1)
@@ -86,59 +98,42 @@ class Board:
     def _get_robot(self, position):
         return self.__get_position(position)["robot"]
 
-    def _get_target(self, position):
-        return self.__get_position(position)["target"]
-    
     def get_target(self):
-        return self.target
+        return self.target[0], self.__position_out(self.target[1])
 
     def _set_barrier(self, barrier, position):
         self.__get_position(position)["barriers"].append(barrier)
 
     def _set_robot(self, robot, position):
         self.__get_position(position)["robot"] = robot
+        self.robots[robot] = position
 
     def _set_target(self, target, position):
-        self.__get_position(position)["target"] = target
         self.target = target, position
 
     def _reset_robot(self, position):
+        robot = self._get_robot(position)
         self.__get_position(position)["robot"] = None
-
-
-    def robot_position(self, robot: str):
-        """ Devolve a posição atual do robô passado como argumento. """
-        for x in range(self.size):
-            for y in range(self.size):
-                if self._get_robot((x, y)) == robot:
-                    return self.__position_out((x, y))
+        self.robots[robot] = None
 
     def _valid_action(self, position, direction):
-        x, y = position
-
-        if direction not in self._get_barriers((x, y)):
-            if direction == "u" and self._get_robot((x - 1, y)) == None:
-                return True
-
-            if direction == "d" and self._get_robot((x + 1, y)) == None:
-                return True
-
-            if direction == "l" and self._get_robot((x, y - 1)) == None:
-                return True
-
-            if direction == "r" and self._get_robot((x, y + 1)) == None:
-                return True
-
+        aux_pos = self.__position_change(position, direction)
+        if direction not in self._get_barriers(position) and self._get_robot(aux_pos) == None:
+            return True                
         return False
 
+    def robot_position(self, robot: str):
+        """ Devolve a posição atual do robô passado como argumento. """     
+        return self.__position_out(self.robots[robot])
+    
 
     def robot_valid_actions(self, robot):
         """ Devolve as possiveis ações do robô passado como argumento. """
-        x, y = self.__position_in(self.robot_position(robot))
+        pos = self.__position_in(self.robot_position(robot))
 
         possible_actions = ("u", "d", "l", "r")
 
-        actions = [(robot, ac) for ac in possible_actions if self._valid_action((x, y), ac)]
+        actions = [(robot, ac) for ac in possible_actions if self._valid_action(pos, ac)]
 
         return actions
 
@@ -148,23 +143,14 @@ class Board:
         position = self.__position_in(self.robot_position(robot))
         self._reset_robot(position)
 
-        position = [position[0], position[1]]
         while(self._valid_action(position, direction)):
-            if direction == "u":
-                position[0] -= 1
-            if direction == "d":
-                position[0] += 1
-            if direction == "l":
-                position[1] -= 1
-            if direction == "r":
-                position[1] += 1
+            position = self.__position_change(position, direction)
         
         self._set_robot(robot, position)
-        position = self.robot_position(robot)
 
     def robot_check_target(self):
-        x, y = self.__position_in(self.robot_position(self.target[0]))
-        return self._get_target((x,y)) == self._get_robot((x,y))
+        tg = self.get_target()
+        return tg[1] == self.robot_position(tg[0])
 
 
 def parse_instance(filename: str) -> Board:
@@ -236,10 +222,8 @@ class RicochetRobots(Problem):
 
     def h(self, node: Node):
         """ Função heuristica utilizada para a procura A*. """
-        board = node.state.board
-        
-        color, target_pos = board.get_target()
-        robot_pos = board.robot_position(color)
+        color, target_pos = node.state.board.get_target()
+        robot_pos = node.state.board.robot_position(color)
 
         return math.sqrt(math.pow((target_pos[0] - robot_pos[0]), 2) + math.pow((target_pos[1] - robot_pos[1]), 2))
 
