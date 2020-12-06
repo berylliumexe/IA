3330a9e3ac634923dd23fb9eb9ff527c7212e53c
nfnvsc <nfnvsc@hotmail.com>
2020-12-06 16:55:03 +0000

    prog

--- 2proj/solutions/alxxx.py
+++ 2proj/solutions/alxxx.py
@@ -30,44 +30,67 @@ class Node:
         self.right = node
 """
 
-def sameClassification(Y):
-    return np.all(Y == Y[0])
+def sameClassification(attributes):
+    return np.all(attributes == attributes[0])
 
-def maxVals(D):
-    count = np.bincount(D)
+def maxVals(examples):
+    count = np.bincount(examples.reshape(-1))
     return np.argmax(count)
 
-def chooseAttribute(attributes, examples):
-    bestAtt = 0
-    bestRatio = 0
-    for i, att in enumerate(attributes.T):
-        posAtt = np.count_nonzero(np.take(examples, np.where(att == 1), axis=0))
-        negAtt = np.count_nonzero(np.take(examples, np.where(att == 0), axis=0))
-
-        posRatio = abs(posAtt - examples.shape[0]/2)
-        negRatio = abs(negAtt - examples.shape[0]/2)
-
-        totRatio = posRatio + negRatio
-
-        if (totRatio > bestRatio):
-            bestAtt = i
-            bestRatio = totRatio
+def GI(attribute, examples):
+    def I(p, n):
+        return - p*np.log2(p) - n*np.log2(n) if p != 0 and n != 0 else 0
     
-    return bestAtt
+    g = 0
+    for v in np.unique(attribute):
+        idxs = np.where(attribute == v)
+        idxs = [i for i in idxs[0] if i < len(examples)]
+        elems = np.take(examples, idxs, axis=0)
+        p = np.count_nonzero(elems)
+        n = len(elems) - p
+        g += (len(elems)/len(examples)) * I(p, n)
 
-def createdecisiontree(D,Y, noise = False):
-    if (D == []):
-        return noise
-    elif (sameClassification(Y)):
-        return Y[0]
-    elif (Y == []):
-        return maxVals(D)
+    return 1 - g
+
+
+def chooseAttribute(attributes, examples, taken_atts):
+    arr = []
+    print(attributes)
+    print(examples)
+    print(taken_atts)
+    for i, att in enumerate(attributes.T):
+        print(att)
+        arr += [GI(att, examples)] if i not in taken_atts else [-1]
+    
+    print(arr)
+    return np.argmax(arr)
+    #return np.argmax(GI(att, examples) if i not in taken_atts else [0] for i, att in enumerate(attributes.T))
+
+def DTL(examples, attributes, default, taken_atts):
+    if (not examples.any()):
+        return maxVals(default)
+    elif (sameClassification(attributes)):
+        return attributes[0]
+    elif (not attributes.any()):
+        return maxVals(examples)
     else:
-        best = chooseAttribute(Y, D)
+        print("---------")
+        best = chooseAttribute(attributes, examples, taken_atts)
         tree = []
-        for v in (0, 1):
-            examplesi = np.take(Y, np.where(D == 1), axis=0)
-            subtree = createdecisiontree(examplesi, Y[:best] + Y[best+1:], maxVals(Y))
+        taken_atts += [best]
+        for v in np.unique(attributes.T[best]):
+            print(f"best: {best}")
+
+            valid_idx = np.where(attributes.T[best] == v)
+
+            print(f"valid_idx: {valid_idx}")
+
+            exs = np.take(examples, valid_idx).reshape(-1)
+
+            #atts = np.concatenate((attributes.T[:best] ,attributes.T[best + 1:])).T
+            
+            #print(f"atts: {atts}")
+            subtree = DTL(exs, attributes, default, taken_atts)
             tree = best, v, subtree
 
     return tree
@@ -79,13 +102,15 @@ if __name__ == "__main__":
                     [1, 0],
                     [1, 1]])        
                  
-    examples = np.array([1,0,1,1])
+    examples = np.array([0,0,0,1])
 
-    print(att.T)
-    idx = np.where(att.T[1] == 0)
-    print(idx)
-    vals = np.take(examples, idx, axis=0)
-    print(vals)
-    print(np.count_nonzero(vals))
+    print(DTL(examples, att, examples, []))
+
+    #print(att.T)
+    #idx = np.where(att.T[1] == 0)
+    #print(idx)
+    #vals = np.take(examples, idx, axis=0)
+    #print(vals)
+    #print(np.count_nonzero(vals))
 
     chooseAttribute(att, examples)
